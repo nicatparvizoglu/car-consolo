6928583e0fd65a3b3fa4c5b4475dbc8f64eb1623
Eziko <58985467+AzizAmiraslanov@users.noreply.github.com>
2020-02-29 19:39:56 +0400

    Add files via upload

---
+++ Console-K101Cars (27.02.2020)/Console-K101Cars/Cars.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_K101Cars
+{
+    class Cars
+    {
+        public string Marka;
+        public string Model;
+        public int FuelPower;
+        public double FuelKm;
+        public double CurrentFuel;
+        public double LocalKM;
+        public double globalKm;
+
+        public Cars(string Marka,string Model,int FuelPower,double FuelKm,double CurrentFuel=40)
+        {
+            this.Marka = Marka;
+            this.Model = Model;
+            this.FuelKm = FuelKm;
+            this.FuelPower = FuelPower;
+            this.CurrentFuel = CurrentFuel;
+        }
+
+           public void Go()
+            {
+            double neededKm = 0;
+
+            while (neededKm == 0) 
+             {
+                Console.WriteLine("Nece km yol getmek isteyirsiniz?");
+                string userInput = Console.ReadLine();
+                if (IsEmpty(userInput))
+                    {
+                    neededKm = Convert.ToDouble(userInput);
+                    if(CurrentFuel>=(neededKm/100)*FuelKm)
+                    {
+                        CurrentFuel-=(neededKm / 100) * FuelKm;
+                        Console.WriteLine($"Siz {neededKm}km qeder yol getdiniz.{CurrentFuel}l benziniz qaldi;");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sizin masinda kifayet qeder benzin yoxdur! Zehmet olmasa benzin doldurun");
+                    }
+                }
+                }
+            }
+        public void TopUp()
+        {
+            Console.WriteLine("nece l benzin yuklemek isteyirsiniz?");
+            string user = Console.ReadLine();
+
+        }
+        bool IsEmpty(string soz)
+        {
+            try
+            {
+                Convert.ToDouble(soz);
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Zehmet olmasa reqem daxil edin");
+                return false;
+            }
+        }
+    }
+}
---
+++ Console-K101Cars (27.02.2020)/Console-K101Cars/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_K101Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car = new Cars("Bmw","M5",80,15,50);
+            Cars mer = new Cars("Mercedes", "E220", 80, 12);
+          
+            string input;
+            Console.WriteLine("***Please write only bottom number***");
+            do
+            {
+                Console.WriteLine("1.Go");
+                Console.WriteLine("2.Top Up");
+                Console.WriteLine("3.Stop");
+                Console.WriteLine("4.Exit");
+                Console.Write(">>>>>>>>>>");
+                input = Console.ReadLine();
+                
+                if (IsEmpty(input))
+                {
+                    switch (input)
+                    {
+                        case "1":
+                            car.Go();
+                            break;
+
+                        case "2":
+                            car.TopUp();
+                            break;
+                        case "3":
+                            Console.WriteLine("Masin gedr");
+                            break;
+                        case "4":
+                            Console.WriteLine("Xidmetlerimizden istifade etdiyiniz üçün teşekkür edirik!");
+                            break;
+                        default:
+                            Console.WriteLine("Please write only top numeric number!");
+                            break;
+                    }
+                }
+            } while (input!="4");
+
+
+
+        }
+        static bool IsEmpty(string soz)
+        {
+            try
+            {
+                Convert.ToDouble(soz);
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Zehmet olmasa reqem daxil edin");
+                return false;
+            }
+        }
+    }
+
+
+    static class  Person
+    {
+        public static string Name;
+        public static string Email;
+        //public Person(string Name,string Email)
+        //{
+        //    this.Name = Name;
+        //    this.Email = Email;
+        //    say++;
+        //    Console.WriteLine(say);
+
+        //}
+    }
+}
